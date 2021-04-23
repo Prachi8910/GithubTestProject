65b700ad62d0a94f674e92332790bb8f0533f544
Prachi <prachi.koyande@neosoftmail.com>
2021-04-23 11:27:26 +0530

    First Commit

---
+++ GithubTestProject/src/com/prachi/githubtest/GitHubTestProject.java
@@ -0,0 +1,12 @@
+package com.prachi.githubtest;
+
+public class GitHubTestProject {
+	public static void main(String args[]) {
+		String code="SE";
+		if(code=="SE")
+			System.out.println("Software engineer");
+		else
+			System.out.println("Not a Software engineer");
+	}
+
+}
